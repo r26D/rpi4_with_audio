9e51260a4b2290cd26b8a0b0309940e43a172652
Dirk Elmendorf <dirk@r26d.com>
2023-08-31 13:23:28 -0500

    Working on getting mpv included in the process. Based on work from https://github.com/nathanl/lullabeam_rpi3/tree/master

--- mix.exs
+++ mix.exs
@@ -1,8 +1,8 @@
-defmodule NervesSystemRpi4.MixProject do
+defmodule Rpi4WithAduio.MixProject do
   use Mix.Project
 
-  @github_organization "nerves-project"
-  @app :nerves_system_rpi4
+  @github_organization "r26D"
+  @app :rpi4_with_audio
   @source_url "https://github.com/#{@github_organization}/#{@app}"
   @version Path.join(__DIR__, "VERSION")
            |> File.read!()
